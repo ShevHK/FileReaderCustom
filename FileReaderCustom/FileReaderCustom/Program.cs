a5535850c33b1c191d25ef55a6cf159f77eafe94
Shev <hksheva@gmail.com>
2023-08-23 21:53:28 +0300

    Create/Finish

---
+++ FileReaderCustom/FileReaderCustom/Program.cs
@@ -0,0 +1,14 @@
+﻿using FileReaderCustom;
+
+string filePath = "./Files/txt.txt"; // Замініть на шлях до вашого текстового файлу
+int validPasswordsCount = 0;
+
+foreach (string line in File.ReadLines(filePath))
+{
+    if (RegexChecker.IsValidPassword(line))
+    {
+        validPasswordsCount++;
+    }
+}
+
+Console.WriteLine($"Number of valid passwords: {validPasswordsCount}");
---
+++ FileReaderCustom/FileReaderCustom/RegexChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FileReaderCustom
+{
+    public static class RegexChecker
+    {
+        public static bool IsValidPassword(string input)
+        {
+            Match match = Regex.Match(input, @"([a-z]) (\d+)(?:-(\d+))?: ([a-z]+)");
+            if (match.Success)
+            {
+                char requiredChar = match.Groups[1].Value[0];
+                int minCount = int.Parse(match.Groups[2].Value);
+                int maxCount = match.Groups[3].Success ? int.Parse(match.Groups[3].Value) : minCount;
+                string password = match.Groups[4].Value;
+
+                int charCount = password.Split(requiredChar).Length - 1;
+                return charCount >= minCount && charCount <= maxCount;
+            }
+
+            return false;
+        }
+    }
+}
